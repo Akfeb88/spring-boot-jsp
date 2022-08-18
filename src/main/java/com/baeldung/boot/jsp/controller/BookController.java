a4648a7aa336adcd4053fda1bf8929afdb2faa84
Akfeb88@gmail.com <Akfeb88@gmail.com>
2022-08-19 00:38:22 +0530

    jjb

--- src/main/java/com/baeldung/boot/jsp/controller/BookController.java
+++ src/main/java/com/baeldung/boot/jsp/controller/BookController.java
@@ -19,7 +19,7 @@ public class BookController {
     private final BookService bookService;
 
     public BookController(BookService bookService) {
-        this.bookService = bookService;
+        this.bookService = bookService; 
     }
 
     @GetMapping("/viewBooks")
